749250fc547a71d790b4632733cebbad5dce347e
liaoxuezhi <liaoxuezhi@baidu.com>
2014-06-19 10:30:35 +0800

    add bigpipe demo

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,16 @@
 module.exports = function( router ) {
 
     router.get('/', function(req, res) {
+        res.bigpipe.bind('pageletA', function(cb) {
+            var model = {
+                content: 'hello world'
+            };
+
+            setTimeout(function() {
+                cb(null, model);
+            }, 2000);
+        });
+
         res.render('example/index.tpl', {
             title: 'Welcome to yogurt!'
         });
