ece96aef8cb0789c3a2f30e0285d2404d2c97c03
liaoxuezhi <liaoxuezhi@baidu.com>
2014-06-19 19:19:54 +0800

    add demo

--- controllers/index.js
+++ controllers/index.js
@@ -3,12 +3,32 @@ module.exports = function( router ) {
     router.get('/', function(req, res) {
         res.bigpipe.bind('pageletA', function(cb) {
             var model = {
-                content: 'hello world'
+                content: 'pagelet A model'
             };
 
             setTimeout(function() {
                 cb(null, model);
             }, 2000);
+        });
+
+        res.bigpipe.bind('pageletB', function(cb) {
+            var model = {
+                content: 'pagelet B model'
+            };
+
+            setTimeout(function() {
+                cb(null, model);
+            }, 5000);
+        });
+
+        res.bigpipe.bind('pageletC', function(cb) {
+            var model = {
+                content: 'pagelet C model'
+            };
+
+            setTimeout(function() {
+                cb(null, model);
+            }, 3000);
         });
 
         res.render('example/index.tpl', {
