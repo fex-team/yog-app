9f58175c0c61703bcdc54edecc5758e576be9b1e
liaoxuezhi <liaoxuezhi@baidu.com>
2014-07-01 12:52:05 +0800

    update yogurt demo

--- controllers/index.js
+++ controllers/index.js
@@ -1,35 +1,40 @@
 module.exports = function( router ) {
 
     router.get('/', function(req, res) {
-        res.bigpipe.bind('pageletA', function(cb) {
-            var model = {
-                content: 'pagelet A model'
-            };
-
+        res.bigpipe.bind('jumbotron', function(cb) {
             setTimeout(function() {
-                cb(null, model);
+                cb(null);
+            }, 1000);
+        });
+
+        
+        res.bigpipe.bind('heding1', function(cb) {
+            setTimeout(function() {
+                cb(null, {
+                    title: 'Pagelet A',
+                    content: 'Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.'
+                });
             }, 2000);
         });
 
-        res.bigpipe.bind('pageletB', function(cb) {
-            var model = {
-                content: 'pagelet B model'
-            };
-
+        res.bigpipe.bind('heding2', function(cb) {
             setTimeout(function() {
-                cb(null, model);
-            }, 5000);
-        });
-
-        res.bigpipe.bind('pageletC', function(cb) {
-            var model = {
-                content: 'pagelet C model'
-            };
-
-            setTimeout(function() {
-                cb(null, model);
+                cb(null, {
+                    title: 'Pagelet B',
+                    content: 'Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.'
+                });
             }, 3000);
         });
+
+        res.bigpipe.bind('heding3', function(cb) {
+            setTimeout(function() {
+                cb(null, {
+                    title: 'Pagelet C',
+                    content: 'Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.'
+                });
+            }, 4000);
+        });
+
 
         res.render('example/index.tpl', {
             title: 'Welcome to yogurt!'
